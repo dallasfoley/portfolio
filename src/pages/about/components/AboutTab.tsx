0d529648748e468312603a8d0fe6b05698450969
dallasfoley <dallastfoley@gmail.com>
2024-08-06 15:45:52 -0400

    update p2

--- src/pages/about/components/AboutTab.tsx
+++ src/pages/about/components/AboutTab.tsx
@@ -3,16 +3,16 @@ const AboutTab = () => {
     <div className="flex items-center justify-center h-full p-5">
       <p className="text-black text-sm md:text-md lg:text-lg leading-relaxed max-w-2xl">
         Hi, I'm Dallas, a recent MSU graduate who studied Mathematics and
-        Computer Science and now specializes in frontend web development. I
-        enjoy building websites in React.js with TypeScript and prefer Tailwind
-        as a styling solution for its flexibility and ease of customization. I
-        am also very interested in the union of static site generation and
-        server-side rendering and particularly how it can enhance performance
-        and search engine optimization and have hence been delving into Next.js.
-        In my free time, I enjoy playing guitar and channeling my creativity
-        into songwriting. I also enjoy diving into the competitive world of
-        League of Legends and spending quality time with my friends and cat,
-        Piplup.
+        Computer Science and now specializes in fullstack web development. I
+        enjoy building websites in React.js with TypeScript on the frontend and
+        prefer Tailwind as a styling solution for its flexibility and ease of
+        customization. I've spent most my time with backend development using
+        Spring Boot as it is very common for many companies and I enjoy the ease
+        of creating robust backends with the Spring Inversion of Control
+        Container. In my free time, I enjoy playing guitar and channeling my
+        creativity into songwriting. I also enjoy diving into the competitive
+        world of League of Legends and spending quality time with my friends and
+        cat, Piplup.
       </p>
     </div>
   );
