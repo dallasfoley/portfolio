d6453d7a5b74842f8462a8c1bde370c9ea22a11f
dallasfoley <dallastfoley@gmail.com>
2024-08-07 15:53:21 -0400

    update p3

--- src/pages/about/components/SkillsTab.tsx
+++ src/pages/about/components/SkillsTab.tsx
@@ -15,6 +15,11 @@ import {
   SiCplusplus,
   SiSpring,
   SiSpringboot,
+  SiXaml,
+  SiEslint,
+  SiPrettier,
+  SiMysql,
+  SiPostgresql,
 } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
 
@@ -26,15 +31,17 @@ const SkillsTab = () => {
           Programming Languages
         </p>
         <div
-          className="grid grid-cols-6 text-2xl md:text-3xl 
+          className="grid grid-cols-8 text-2xl md:text-3xl 
         lg:text-5xl gap-5  md:gap-10"
         >
-          <SiC color="black"></SiC>
-          <SiCplusplus color="black"></SiCplusplus>
-          <SiJavascript color="black" className="" />
-          <SiTypescript color="black" className="" />
-          <SiPython color="black" className="" />
-          <FaJava color="black"></FaJava>
+          <SiC color="black" title="C"></SiC>
+          <SiCplusplus color="black" title="C++"></SiCplusplus>
+          <SiJavascript color="black" title="JavaScript" className="" />
+          <SiTypescript color="black" title="TypeScript" className="" />
+          <SiPython color="black" title="Python" className="" />
+          <FaJava color="black" title="Java"></FaJava>
+          <SiMysql color="black" title="MySQL"></SiMysql>
+          <SiPostgresql color="black" title="PostgreSQL"></SiPostgresql>
         </div>
       </div>
       <div>
@@ -42,29 +49,35 @@ const SkillsTab = () => {
           Markup/Styling Languages
         </p>
         <div className="grid grid-cols-3 gap-8 md:gap-12 text-2xl md:text-3xl lg:text-5xl">
-          <SiHtml5 color="black" />
-
-          <SiCss3 color="black" />
-          <SiTailwindcss color="black" />
+          <SiHtml5 color="black" title="HTML5" />
+          <SiCss3 color="black" title="CSS3" />
+          <SiXaml color="black" title="XML/XAML" />
+        </div>
+      </div>
+      <div>
+        <p className="text-blue-600 text-center text-lg md:text-2xl my-7">
+          Libraries and Frameworks
+        </p>
+        <div className="grid grid-cols-5 gap-8 md:gap-12 text-2xl md:text-3xl lg:text-5xl">
+          <SiSpring color="black" title="Java Spring"></SiSpring>
+          <SiSpringboot color="black" title="Spring Boot"></SiSpringboot>
+          <SiReact color="black" title="React.js" className="" />
+          <SiNextdotjs color="black" title="Next.js" />
+          <SiTailwindcss color="black" title="Tailwind CSS" />
         </div>
       </div>
       <div className="flex flex-col items-center">
         <p className="text-blue-600 text-center text-lg md:text-2xl my-7">
-          Libraries, Frameworks and Technologies
+          Technologies
         </p>
 
-        <div
-          className="flex justify-between items-center text-2xl md:text-3xl 
-          lg:text-5xl w-full md:gap-10 text-black font-bold"
-        >
-          <SiSpring color="black"></SiSpring>
-          <SiSpringboot color="black"></SiSpringboot>
-          <SiReact color="black" className="" />
-          <SiNextdotjs color="black" className="" />
-          <SiGit color="black" className="" />
-          <SiVite color="black" className="" />
-          <SiVercel color="black" />
-          <SiLatex color="black" className="" />
+        <div className="grid grid-cols-6 gap-6 md:gap-12 text-2xl md:text-3xl lg:text-5xl">
+          <SiGit color="black" title="Git" className="" />
+          <SiVite color="black" title="Vite" className="" />
+          <SiVercel color="black" title="Vercel" />
+          <SiEslint color="black" title="ESLint" />
+          <SiPrettier color="black" title="Prettier" />
+          <SiLatex color="black" title="LaTeX" className="" />
         </div>
       </div>
     </div>
